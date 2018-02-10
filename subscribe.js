59836e8c585ea5bd0c7c2a9887e9c923d11068c5
jervis446 <kumar.adarshluv99@gmail.com>
2018-02-11 00:22:51 +0530

    update

--- subscribe.js
+++ subscribe.js
@@ -1,113 +1,61 @@
-var PRESSED=false;
-$('.subscribe').on('click',function(){
-	PRESSED=true;
-	
-	$( "#innercontent" ).fadeOut( "slow", function() {
-	$("#content").html("<div class='messages'>Enter Email to get notified When new Though Float on to the Sky!</div>","<div  class='form-control' placeholder='Enter Email here ...'></textarea><br><input type='checkbox' checked> Allow comments<span style='float:right'><span onclick='submit();' class='msgbtn'>Submit</span> | <span onclick='skip(false);' class='msgbtn'>Skip</span></span>");
-});
-});
-/*
-
-$('body').keyup(function(e){
-   if(e.keyCode == 32 && PRESSED && o<ttt.length){
-       // user has pressed space
-
-    $('#content').fadeOut('slow', function() {
-    $('#content').html("<div class='messages col-md-6 col-sm-12 col-xs-12'>"+ttt[o]+"</div>");
-    $('#content').fadeIn('slow');
-     o++;
-});
-
- }
-});
-
-
-window.addEventListener('touchstart', function() {
-  if(PRESSED && o<ttt.length){
-  	$('#content').fadeOut('slow', function() {
-    $('#content').html("<div class='messages'>"+ttt[o]+"</div>");
-    $('#content').fadeIn('slow');
-     o++;
-  });
-  }
-});
-
-
-function submit(){
-var databaseref = firebase.database().ref();
-var thoughtsref=databaseref.child('email');
-var jjj=new Date.getTime();
-var data={
-	"thought":$('textarea').val(),
-	"allowcomments":$('input[type="checkbox"]').is(":checked"),
-	"tim":jjj
-}
-thoughtsref.push(data);
-skip(true);
+var mover=function(msg,com,ki,coms){
+	this.size=random(6);
+	this.cx=random(-width*1.5,width);
+	this.cy=random(-height*1.5,height*2);
+	this.dispx=0;
+	this.dispy=0;
+	this.dep=random(20);
+	this.seed=parseInt(random(2000));
 }
 
-var ymover=function(d){
-	this.y=height*0.7+d;
-	this.x=0.4*width;
-}
-ymover.prototype.move=function(){
-	this.x+=0.5;
-}
-ymover.prototype.show=function(){
-	stroke('#f2ca66');
-	strokeWeight(0.7);
-	noFill();
-	var si=map(abs(frameCount%20-10),0,10,4,8);
-	if(this.x<width)ellipse(this.x,this.y,si,si);
-	noStroke();
-}
-var mover=function(){
-	this.radius=Math.random()*5;
-	this.posx=Math.random()*width;
-	this.posy=Math.random()*1000;
-	this.vel=Math.random()*1;
-}
 mover.prototype.move=function(){
-	this.posx+=this.vel;
-	//#f2ca66
+	this.cx+=(noise(this.seed)-0.5)*this.dep/50;
+	this.cy+=(noise(this.seed+5768)-0.5)*this.dep/50;
+	this.dispx=this.dep*map(mouseX,0,width,-1,1);
+	this.dispy=this.dep*map(mouseY,0,height,-1,1);
 }
-mover.prototype.show=function(){
-	if(random(5)<0.5)fill(random(255));
-	else fill(200);
-	ellipse(this.posx,this.posy,this.radius,this.radius);
-	if(this.posx>width)this.posx=0;
 
+
+
+mover.prototype.show=function(){
+	fill("#f2ca66");
+	ellipse(this.cx+this.dispx,this.cy+this.dispy,this.size,this.size);
 }
 var xaxu=[];
-var clicka=[];
+var ruzu=[];
 function setup() { 
+
+textSize(19);
 	noStroke();
 	createCanvas(windowWidth, windowHeight);
-	for(var i=0;i<200;i++)xaxu.push(new mover());
-	
+
+
+for(var i=0;i<600;i++)xaxu.push(new mover());
+
 }
+var angle=0;var twist=false;
 function draw() {
 	background(20);
-	
-	for(var i=0;i<200;i++){
+	translate(width*0.7,height/2);
+	rotate(angle);
+	for(var i=0;i<600;i++){
 		xaxu[i].move();
 		xaxu[i].show();
 	}
-		for(var j=0;j<clicka.length;j++){
-		clicka[j].move();
-		clicka[j].show();
+	if(twist==true){
+		angle+=0.1;
+		if(angle>TWO_PI){
+			twist=false;
+			angle=0;
+		}
 	}
-	dispY=map(sin(frameCount/80),-1,1,-90,90);
-
-	fill('#f2ca66');
-	ellipse(0.4*width,height*0.7+dispY,10,10);
-	if(PRESSED && frameCount%10==0)clicka.push(new ymover(dispY));
+	noFill();
+	strokeWeight(2);
+	stroke("#f2ca66");
+	ellipse(0,0,200,200);
+	noStroke();
+	fill(255);
+	for(var i=0;i<ruzu.length;i++)ellipse(100*cos(ruzu[i]),100*sin(ruzu[i]),12,12);
 
 
 }
-function mousePressed(){
-clicka.push(new ymover(dispY));
-}
-function windowResized() {
-	resizeCanvas(windowWidth, windowHeight);
-}*/
